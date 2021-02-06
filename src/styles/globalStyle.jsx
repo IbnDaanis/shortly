05f9ea0361f47dfdbf987f21d5793eb2e28d4335
Ikraam <IbnDaanis@Gmail.com>
2021-02-06 16:52:10 +0000

    Added style variables

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -1,11 +1,12 @@
-import { createGlobalStyle } from 'styled-components/macro'
+import { createGlobalStyle } from 'styled-components/'
+import { secondary } from './styleVariables'
 
 const GlobalStyle = createGlobalStyle`
  * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Avenir', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+  font-family: 'Poppins','Avenir', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-appearance: none;
   -moz-appearance: none;
---
+++ src/styles/styleVariables.jsx
@@ -0,0 +1,15 @@
+export const primary = {
+  cyan: '#2acfcf',
+  darkViolet: '#3b3054',
+}
+
+export const secondary = {
+  red: 'rgb(244, 98, 98)',
+}
+
+export const neutral = {
+  gray: 'hsl(0, 0%, 75%)',
+  grayishViolet: 'hsl(257, 7%, 63%)',
+  veryDarkBlue: 'hsl(255, 11%, 22%)',
+  veryDarkViolet: 'hsl(260, 8%, 14%)',
+}
