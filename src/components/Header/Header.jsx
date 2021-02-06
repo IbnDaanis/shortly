7551e59b74f0dc40946a298fff468a8556fc0735
Ikraam <IbnDaanis@Gmail.com>
2021-02-06 18:33:45 +0000

    Added mobile styling for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import { useState } from 'react'
 import {
   HeaderContainer,
   Container,
   LoginSignUp,
   Logo,
   NavLinks,
+  MenuIcon,
+  Menu,
 } from './HeaderStyles'
+import { HiMenu } from 'react-icons/hi'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <HeaderContainer>
       <nav>
         <Container>
           <Logo src='./images/logo.svg' />
-          <NavLinks>
-            <li>
-              <a href='#'>Features</a>
-            </li>
-            <li>
-              <a href='#'>Pricing</a>
-            </li>
-            <li>
-              <a href='#'>Resources</a>
-            </li>
-          </NavLinks>
-          <LoginSignUp>
-            <a href='#'>Login</a>
-            <button>Sign Up</button>
-          </LoginSignUp>
+          <Menu isMenuOpen={isMenuOpen}>
+            <NavLinks>
+              <li>
+                <a href='#'>Features</a>
+              </li>
+              <li>
+                <a href='#'>Pricing</a>
+              </li>
+              <li>
+                <a href='#'>Resources</a>
+              </li>
+            </NavLinks>
+            <LoginSignUp>
+              <a href='#'>Login</a>
+              <button>Sign Up</button>
+            </LoginSignUp>
+          </Menu>
+          <MenuIcon onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)}>
+            <HiMenu />
+          </MenuIcon>
         </Container>
       </nav>
     </HeaderContainer>
--- src/components/Header/HeaderStyles.jsx
+++ src/components/Header/HeaderStyles.jsx
@@ -11,15 +11,39 @@ export const HeaderContainer = styled.header`
 export const Container = styled.div`
   margin: 0 auto;
   width: 1100px;
+  max-width: 95%;
   height: 130px;
   display: flex;
   align-items: center;
   font-size: 15px;
+  position: relative;
+  @media (max-width: 1000px) {
+    justify-content: space-between;
+    font-size: 18px;
+  }
+`
+
+export const Menu = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 1000px) {
+    display: ${props => (props.isMenuOpen ? 'flex' : 'none')};
+    position: absolute;
+    background: ${primary.darkViolet};
+    flex-direction: column;
+    border-radius: 12px;
+    top: 100px;
+    max-width: 500px;
+    right: 0;
+  }
 `
 
 export const Logo = styled.img`
   margin-right: 2.5rem;
   color: ${primary.darkViolet};
+  cursor: pointer;
 `
 
 export const NavLinks = styled.ul`
@@ -28,20 +52,45 @@ export const NavLinks = styled.ul`
   align-items: center;
   list-style: none;
   width: 260px;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    padding: 0.5rem 0 1rem;
+    margin-bottom: 1.5rem;
+    border-bottom: 2px solid #494161;
+    width: 300px;
+    max-width: 90%;
+  }
+
   li {
     font-weight: 700;
     color: ${neutral.grayishViolet};
     cursor: pointer;
     transition: color 0.2s ease-in-out;
+
+    @media (max-width: 1000px) {
+      color: white;
+      padding: 0.75rem 0;
+    }
     &:hover {
       color: ${neutral.veryDarkBlue};
+      @media (max-width: 1000px) {
+        color: ${neutral.grayishViolet};
+      }
     }
   }
 `
 export const LoginSignUp = styled.div`
-  margin-left: auto;
   display: flex;
   align-items: center;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    padding: 0.5rem 0 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+  }
+
   a {
     font-weight: 700;
     color: ${neutral.grayishViolet};
@@ -49,8 +98,16 @@ export const LoginSignUp = styled.div`
     cursor: pointer;
     transition: color 0.2s ease-in-out;
 
+    @media (max-width: 1000px) {
+      color: white;
+      margin: 0 0 1rem;
+    }
+
     &:hover {
       color: ${neutral.veryDarkBlue};
+      @media (max-width: 1000px) {
+        color: ${neutral.grayishViolet};
+      }
     }
   }
   button {
@@ -64,5 +121,29 @@ export const LoginSignUp = styled.div`
     border: 0;
     border-radius: 1000px;
     cursor: pointer;
+
+    @media (max-width: 1000px) {
+      font-size: 18px;
+      width: 300px;
+      max-width: 90%;
+    }
+  }
+`
+
+export const MenuIcon = styled.div`
+  display: none;
+  align-items: center;
+  justify-content: flex-end;
+  color: ${neutral.grayishViolet};
+  font-size: 2.5rem;
+  cursor: pointer;
+  transition: color 0.2s ease-in-out;
+  transform: translateX(4px);
+  @media (max-width: 1000px) {
+    display: flex;
+  }
+
+  &:hover {
+    color: ${neutral.veryDarkBlue};
   }
 `
