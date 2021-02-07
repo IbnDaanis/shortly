c1efee13d98eb9b0299041a11bbd16d05708ae86
Ikraam <IbnDaanis@Gmail.com>
2021-02-07 13:39:41 +0000

    Added useFetch and connected to API

--- src/components/UrlInput/UrlInput.jsx
+++ src/components/UrlInput/UrlInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import {
   Container,
   Form,
@@ -8,15 +8,39 @@ import {
 } from './UrlInputStyles'
 
 const UrlInput = () => {
+  const [url, setUrl] = useState(null)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    console.log('Submitted')
+    setLoading(true)
+    const fetchData = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+    const response = await fetchData.json()
+    setData(response.result)
+    setLoading(false)
+  }
+
   return (
     <div>
       <Container>
-        <Form>
+        <Form onSubmit={e => handleSubmit(e)}>
           <FormInputContainer>
-            <FormInput type='text' placeholder='Shorten a link here...' />
+            <FormInput
+              type='text'
+              placeholder='Shorten a link here...'
+              onChange={({ target }) => setUrl(target.value)}
+            />
             <h4>Please add a link</h4>
           </FormInputContainer>
-          <FormButton disabled>Shorten It!</FormButton>
+          <FormButton type='submit'>
+            {loading ? 'Loading' : 'Shorten It!'}
+          </FormButton>
         </Form>
       </Container>
     </div>
