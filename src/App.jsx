38d244a12349e3d765174922f0c9e4e0dc64c318
Ikraam <IbnDaanis@Gmail.com>
2021-02-09 14:40:05 +0000

    Added LocomotiveScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Boost, Header } from './components'
 import { Footer, Hero, Shortener, Statistics } from './containers'
 import GlobalStyle from './styles/globalStyle'
+import LocomotiveScroll from 'locomotive-scroll'
 
 const App = () => {
+  const scroll = new LocomotiveScroll({})
   return (
     <div>
       <GlobalStyle />
--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -1,30 +1,10 @@
 import { Container, HeroContainer, HeroCTA, HeroImage } from './HeroStyles'
-import {
-  useViewportScroll,
-  motion,
-  useTransform,
-  useMotionValue,
-} from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
 
 const Hero = () => {
-  const { scrollY } = useViewportScroll()
-  const y1 = useTransform(scrollY, [0, 300], [0, 200])
-  const y2 = useTransform(scrollY, [0, 300], [0, -100])
-
-  const [ref, inView, entry] = useInView({
-    /* Optional options */
-    threshold: 0.5,
-    triggerOnce: false,
-  })
-
   return (
     <HeroContainer>
       <Container>
-        <HeroCTA
-          style={{ y: y2, x: 0 }}
-          transition={{ duration: 2, ease: 'easeOut' }}
-        >
+        <HeroCTA>
           <h1 className='title'>More than just shorter links</h1>
           <p className='description'>
             Build your brand's recognition and get detailed insights on how your
@@ -32,7 +12,7 @@ const Hero = () => {
           </p>
           <button className='cta-button'>Get Started</button>
         </HeroCTA>
-        <HeroImage style={{ y: y2, x: 0 }}>
+        <HeroImage>
           <img src='./images/illustration-working.svg' alt='Hero Image' />
         </HeroImage>
       </Container>
